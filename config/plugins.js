2bd28a68e523d43b61783889879e0388ed809b0b
dag-bag <virenderkumar23435@gmail.com>
2023-07-08 02:43:05 -0700

    Schema Update

--- config/plugins.js
+++ config/plugins.js
@@ -8,5 +8,19 @@ module.exports = {
       apiToken: process.env.OPEN_AI_API_TOKEN,
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
   //...
 };
